bafe6a5f79ae71d075d18574e17de85740ed5d92
João Pedro Vogel Michalski <joaomichalskiti@gmail.com>
2023-09-04 23:16:36 -0300

    .

---
+++ src/app/create/page.tsx
@@ -0,0 +1,26 @@
+import Image from "next/image";
+
+export default function create() {
+  return (
+    <div className=" min-h-screen w-full bg-black flex  items-center justify-center bg-contain bg-[url('/espaço.jpg')]">
+      <main className="flex flex-col h-80 items-center">
+        <Image
+          className=" mb-3"
+          alt="oi"
+          src="/world.gif"
+          width={100}
+          height={100}
+        />
+        <form action="" className="flex flex-col gap-y-4 mt-5 ">
+          <input className="h-8  " type="text" name="nome" id="nome" />
+          <input className="h-8  " type="email" />
+          <input className="h-8  " type="password" />
+          <div className=" flex justify-center w-full flex-col">
+            <hr className="w-full mt-4" />
+            <button className="font-thin text-white hover:text-zinc-400 active:text-white mt-3">CREATE ACOUNT</button>
+          </div>
+        </form>
+      </main>
+    </div>
+  );
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,37 +1,50 @@
-import Image from 'next/image';
-import React from 'react';
+import Image from "next/image";
+import React from "react";
+import Link from "next/link";
 
 export default function Home() {
   return (
-  <div className="ola min-h-screen w-full bg-black flex  items-center justify-center bg-[url('/espaço.jpg')]">
-   <main className='flex flex-col justify-center items-center '>
-      <div className=''>
-        <Image
-          alt='oi'
-          src="/world.gif"
-          width={100}
-          height={100}  
-        />
-      </div>
-      <section className='flex-initial h-80 w-full  mt-10 '>
-        <div className=' w-full flex justify-center mt-3 font-thin text-2xl'>
-          <h1 className=' text-white '>WELCOME</h1>
+    <div className=" min-h-screen w-full bg-black flex  items-center bg-contain justify-center bg-[url('/espaço.jpg')]">
+      <main className="flex flex-col justify-center items-center ">
+        <div className="">
+          <Image alt="oi" src="/world.gif" width={100} height={100} />
         </div>
-        <div className=' w-full flex flex-col items-center justify-center mt-4'>
-          <input className='m-2 w-4/5 p-1' type="text" name="email" id="email" />
-          <input className='m-2 w-4/5 p-1' type="password" name="sennha" id="senha" />
-        </div>
-        <div className=' w-full flex justify-center'>
-          <button className=' mt-4 font-thin text-white hover:text-zinc-400 active:text-white'>SING IN</button>
-        </div>
-        <div className=' mt-4 w-full flex justify-center'>
-        <hr className=' w-11/12'/>
-        </div>
-        <div className=' mt-4 h-1/5 w-full flex justify-center items-center'>
-          <button className=' text-white font-thin hover:text-zinc-400 active:text-white'>CREATE ACCOUNT</button>
-        </div>
-      </section>
-    </main>
-  </div>
-  )
+        <section className="flex-initial h-80 w-full  mt-10 ">
+          <div className=" w-full flex justify-center mt-3 font-thin text-2xl">
+            <h1 className=" text-white ">WELCOME</h1>
+          </div>
+          <div className=" w-full flex flex-col items-center justify-center mt-4">
+            <input
+              className="m-2 w-4/5 h-8 p-1 text-sm/[6px] font-bold "
+              type="email"
+              name="email"
+              id="email"
+            />
+            <input
+              className="m-2 w-4/5 h-8 p-1 "
+              type="password"
+              name="sennha"
+              id="senha"
+            />
+          </div>
+          <div className=" w-full flex justify-center">
+            <button className=" mt-4 font-thin text-white hover:text-zinc-400 active:text-white">
+              SING IN
+            </button>
+          </div>
+          <div className=" mt-4 w-full flex justify-center">
+            <hr className=" w-11/12" />
+          </div>
+          <div className=" mt-4 h-1/5 w-full flex justify-center items-center">
+            <Link
+              className=" text-white font-thin hover:text-zinc-400 active:text-white"
+              href="./create"
+            >
+              CREATE ACCOUNT
+            </Link>
+          </div>
+        </section>
+      </main>
+    </div>
+  );
 }
